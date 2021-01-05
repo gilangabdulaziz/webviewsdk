52b5b0b44dd83ce8585e3a6d0d78aa4928f2de31
gilang <gilangabdulaziz@live.com>
2021-01-05 11:10:32 +0700

    remove clear flags..

--- ymwebview/src/main/java/com/example/ymwebview/BotWebView.java
+++ ymwebview/src/main/java/com/example/ymwebview/BotWebView.java
@@ -114,11 +114,6 @@ public class BotWebView extends AppCompatActivity {
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 Window window = BotWebView.this.getWindow();
-                // clear FLAG_TRANSLUCENT_STATUS flag:
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                // finally change the color
                 window.setStatusBarColor(customColor);
             }
         }
